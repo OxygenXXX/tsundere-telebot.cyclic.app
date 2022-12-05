ffd6e392e944f71f69b67deebcced5a32c03e57a
OxygenXXX <oxyyygen.developer@gmail.com>
2022-12-05 15:27:04 +0000

    Initial commit

---
+++ client.js
@@ -0,0 +1,26 @@
+const grammy = require("grammy");
+const dotenv = require("dotenv").config();
+
+const client = new grammy.Bot(process.env.CLIENT_TOKEN);
+
+client.command("start", (ctx) => ctx.reply("Hola!"));
+
+client.on("message", (ctx) => ctx.reply("Hello!"));
+
+if (process.env.NODE_ENV == "production")
+{
+    const express = require("express");
+
+    const application = express();
+
+    application.use(express.json());
+
+    application.use(grammy.webhookCallback(client, "express"));
+
+    application.listen(process.env.PORT || 3000, () => console.log("Running"));
+}
+
+else
+{
+    client.start().then(console.log("Started"));
+}
