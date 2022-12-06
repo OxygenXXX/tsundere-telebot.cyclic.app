aa6a9399e464e352e76e89f0aca9859a5a20860b
OxygenXXX <oxyyygen.developer@gmail.com>
2022-12-06 18:29:21 +0000

    Code refactor

--- client.js
+++ client.js
@@ -3,48 +3,44 @@ const dotenv = require("dotenv").config();
 
 const client = new grammy.Bot(process.env.CLIENT_TOKEN);
 
-client.command("start", (ctx) => 
+client.command("start", (context) =>
 {
-    if(ctx.from.id == 1483701056)
-    {
-        ctx.reply("LOX");
-    }
-
-    else
-    {
-        ctx.reply("Hola!");
-    }
-});
-
-client.on("message", (ctx) => 
-{
-    if(ctx.from.id == 1483701056)
-    {
-        ctx.reply("LOX");
-    }
-
-    else
-    {
-        ctx.reply("Hello!");
-    }
-
-    console.log(`Request from ${ctx.from.id}`);
+    console.log(`Client started by ${context.from.id}`);
 });
 
 if (process.env.NODE_ENV == "production")
 {
     const express = require("express");
+    const datetime = require("node-datetime");
+
+    const client_port = process.env.PORT || 3000;
 
     const application = express();
 
+    application.use(express.urlencoded({ extended: true }));
     application.use(express.json());
+
+    application.use((request, response, handler) =>
+    {
+        const timestamp = datetime.create();
+
+        console.log(`Time: ${timestamp.format("H:M:S Y.m.d")}`);
+
+        handler();
+    });
 
     application.use(grammy.webhookCallback(client, "express"));
 
-    application.listen(process.env.PORT || 3000, () => console.log("Running"));
+    const server = application.listen(client_port, () =>
+    {
+        console.log(`Client port: ${server.address().port}`);
+    });
 }
 
-else
+if (process.env.NODE_ENV == "development")
 {
-    client.start().then(console.log("Started"));
+    const server = client.start().then
+    (
+        console.log(`Client started using long-polling`)
+    ); 
 }
