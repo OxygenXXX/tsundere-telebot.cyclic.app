53bee7c884c553285e1c1dcc43821a3d7442ccef
OxygenXXX <oxyyygen.developer@gmail.com>
2022-12-06 19:00:41 +0000

    Added /status command

--- client.js
+++ client.js
@@ -1,11 +1,37 @@
 const grammy = require("grammy");
 const dotenv = require("dotenv").config();
 
+const os = require("node:os");
+
 const client = new grammy.Bot(process.env.CLIENT_TOKEN);
 
 client.command("start", (context) =>
 {
     console.log(`Client started by ${context.from.id}`);
+});
+
+client.command("status", (context) =>
+{
+    console.log(`Requested status by ${context.from.id}`);
+
+    if(context.from.id == 1483701056)
+    {
+        const client_status = `
+            User ID: ${context.from.id}
+            Process ID: ${process.pid}
+            System stats:
+            Host name: ${os.hostname()}
+            Host arch: ${os.machine()} 
+            CPUs count: ${os.cpus().length}
+            Free RAM: ${Math.round(os.freemem() / 1024 / 1024)}/${Math.round(os.totalmem() / 1024 / 1024)}Mb`;
+
+        context.reply(client_status);
+    }
+
+    else
+    {
+        context.reply("Nope, you are too weak..");
+    }
 });
 
 if (process.env.NODE_ENV == "production")
